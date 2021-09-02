75bc5371babb64350f544cd0acde3686db33146d
stephull <shullender0907@gmail.com>
2021-09-01 22:30:52 -0400

    change configurations, import des algorithm

--- lab-1/configurations.py
+++ lab-1/configurations.py
@@ -2,12 +2,19 @@
     configurations.py   -- Includes all constants for all files
 '''
 
-# imports are for ALL files
-import os, sys, errno, random, string
+# imports are for ALL files (include path for external imports)
+import sys, os, errno
+sys.path.append(r'c:/users/shull/appdata/local/packages/pythonsoftwarefoundation.python.3.7_qbz5n2kfra8p0/localcache/local-packages/python37/site-packages')
+
+import random, string
+from des import DesKey
 
 # important attributes or variables
 PORT = 6666
 LOCALHOST = "127.0.0.1"
+
+BACKLOG = 2
+RECV_BYTES = 1024
 
 INPUT_LENGTH = 64
 MAX_PORT = 49151
@@ -15,6 +22,6 @@ MIN_PORT = 1024
 
 DIRECTORY = "lab-1"
 KEYFILE = "key.txt"
+KEY_LEN = 8
 
-MIN_KEY_LEN = 6
-MAX_KEY_LEN = 8
+INPUT_STR = "Type message >>> "
